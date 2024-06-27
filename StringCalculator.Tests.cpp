38e7e6d7e25134724db74cd3e67f1e8843c170c1
gbharathi111 <gauthaman.bharathi@in.bosch.com>
2024-06-27 14:47:33 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -19,7 +19,7 @@ TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     ASSERT_EQ(result, expectedresult);
 }
 
-/*
+
 TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
     std::string input = "1,2";
@@ -62,4 +62,4 @@ TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     int result = objUnderTest.add(input);
 
     ASSERT_EQ(result, expectedresult);
-}*/
+}
